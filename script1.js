530ead4a7916f366a4115caa2f8e53e87ff9fe4f
William Purcell <william@chartbeat.com>
2022-03-10 17:47:31 -0500

    things rolling on a first project

---
+++ script1.js
@@ -0,0 +1,36 @@
+const cavnas = document.getElementById('canvnas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray;
+
+let mouse = {
+    x: null,
+    y: null,
+    radius: (canvas.height/80) * (canvas.width/80);
+}
+
+window.addEventListener('mousemove',
+    function(event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+    }
+);
+
+class Particle {
+    consntructor(x, y, directionX, directionY, size, color) {
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+
+    draw () {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false)
+    }
+    
+}
