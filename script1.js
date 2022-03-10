86bb2751ced22f5fcb6c9df2555cd7c355d7a212
William Purcell <william@chartbeat.com>
2022-03-10 18:22:23 -0500

    framework in place to actually draw stuff

--- script1.js
+++ script1.js
@@ -8,7 +8,7 @@ let particlesArray;
 let mouse = {
     x: null,
     y: null,
-    radius: (canvas.height/80) * (canvas.width/80);
+    radius: (canvas.height/80) * (canvas.width/80)
 }
 
 window.addEventListener('mousemove',
@@ -18,6 +18,7 @@ window.addEventListener('mousemove',
     }
 );
 
+// create particle
 class Particle {
     consntructor(x, y, directionX, directionY, size, color) {
         this.x = x;
@@ -28,9 +29,65 @@ class Particle {
         this.color = color;
     }
 
+    // method to draw individual particles
     draw () {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false)
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = '#8C5523';
+        ctx.fill();
     }
-    
+
+    // check particle position, check mouse positionn, move the particle,
+    // draw the particle
+    update () {
+        if (this.x > canvas.width || this.x < 0) {
+            this.directionX = -this.directionX;
+        }
+        if (this.y > canvas.width || this.y < 0) {
+            this.directionY = -this.directionY;
+        }
+        
+        // check collision detection - mouse position / particle position
+        let dx = mouse.x - this.y;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < mouse.radius + this.size) {
+            if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+                this.x += 10;
+            }
+            if (mouse.x > this.x && this.x > this.size * 10) {
+                this.x -= 10;
+            }
+            if (mouse.y < this.y && this.y < canvas.width - this.size * 10) {
+                this.y += 10;
+            }
+            if (mouse.y > this.y && this.y > this.size * 10) {
+                this.x -= 10;
+            }
+        }
+        // move particle
+        this.x += this.directionX;
+        this.y += this.directionY;
+
+        // draw particle
+        this.draw();
+    }
+}
+
+// create particle array. We first calculate a size between 1-5,
+// then use that number to create a random location on the canvas 
+// and a random direction
+function init () {
+    particlesArray = [];
+    let numberOfParticles = (canvas.height * canvas.width) / 9000;
+    for (let i = 0; i < numberOfParticles; i++) {
+        let size = (Math.random() * 5) + 1;
+        let x = (Math.random() * ((innerWidth - size * 2) - (size * 2) + size * 2));
+        let y = (Math.random() * ((innerWidth - size * 2) - (size * 2) + size * 2));
+        let directionX = (Math.random() * 5) - 2.5;
+        let directionY = (Math.random() * 5) - 2.5;
+        let color = '#8C5523';
+
+        particlesArray.push(new Particle(x, y, directionX, directionY, size, color))
+    }
 }
